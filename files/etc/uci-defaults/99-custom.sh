1abe90e8d69f5ab02f4d785cfd8cc07832f238a2
V1V1V112 <46706221+V1V112@users.noreply.github.com>
2025-08-09 21:39:07 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -36,14 +36,6 @@ ifnames=$(echo "$ifnames" | awk '{$1=$1};1')
 
 # 网络设置
 if [ "$count" -eq 1 ]; then
-    # 单网口设备 类似于NAS模式 动态获取ip模式 具体ip地址取决于上一级路由器给它分配的ip 也方便后续你使用web页面设置旁路由
-    # 单网口设备 不支持修改ip 不要在此处修改ip 单网口采用dhcp模式 删除默认的192.168.1.1
-    # uci set network.lan.proto='dhcp'
-    # uci delete network.lan.ipaddr
-    # uci delete network.lan.netmask
-    # uci delete network.lan.gateway     
-    # uci delete network.lan.dns 
-    # uci commit network
     # LAN口设置静态IP
     uci set network.lan.proto='static'
     uci set network.lan.ipaddr='192.168.2.3'
